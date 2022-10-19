c2195331e29836b52ea6293a386d9872b0587de4
Eyob Jovani <eyobjovan@Eyobs-MacBook-Pro.local>
2022-10-18 20:29:31 -0400

    adding three projects

---
+++ Filing Single Tax.py
@@ -0,0 +1,19 @@
+income = float(input("Your Taxable Income: $"))
+tax = 0.0
+if income <= 9875:
+    tax = (income * .10)
+elif income <= 40125:
+    tax = 987.50 + .12 * (income - 9875)
+elif income <= 85525:
+    tax = 4617.50 + .22 * (income - 40125)
+elif income <= 163300:
+    tax = 14605.50 + .24 * (income - 85525)
+elif income <= 207350:
+    tax = 33271.50 + .32 * (income - 163300)
+elif income <= 518400:
+    tax = 47367.50 + .35 * (income - 207350)
+else:
+    tax = 156235 + .37 * (income - 518400)
+remainder = income - tax
+print("Your Tax Bill: ${tax}".format(tax = tax),"income taxes\n")
+print("Your Income After Tax: ${rem}".format(rem = remainder),"income\n")
